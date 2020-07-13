6c3c9ee0b4d3789b91e13c0d1380445d7f4184a1
awkwarden <awkwardener@gmail.com>
2020-07-12 20:37:24 -0400

    update readme and createrepo.sh to actually work

--- createrepo.sh
+++ createrepo.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-REPO=${1?Error: no repo name given}
-USER=${2?Error: no user name given}
-mkdir $REPO
-cd $REPO
+USER=${1?Error: no user name given}
+REPO=${2?Error: no repo name given}
+
+mkdir ~/src/$REPO
+cd ~/src/$REPO
+echo "# $REPO" >> README.md
 git init
+pwd
 git add README.md
 git commit -m "auto create"
 git remote add origin git@github.com:$USER/$REPO.git
